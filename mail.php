428c1051d38f365b0e7bec35d2ef36b2435d60f7
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2022-02-07 10:36:38 +0200

    heroku deploy add email contact us

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $email = $_POST['email'];
 
 
 $content="From: $name";
-$recipient = "daniel@byol.ie";
+$recipient = "aa.nazeeh@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
